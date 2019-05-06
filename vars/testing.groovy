3dd34bb148d264ae14f29dc5a2a82f65e45cbf13
Devops_CG <sudhaajan14@gmail.com>
2019-05-06 23:45:12 +0530

    test

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,13 +1,13 @@
 def call(){
     node {
         stage("core artifact") {
-        bat "cd ${WORKSPACE}/core && mvn clean install"
+        bat "cd ${WORKSPACE}/spring-mvc-example && mvn clean install"
     }
 
 
 
         stage("data-Build") {
-        bat "cd ${WORKSPACE}/core && mvn clean install"
+        bat "cd ${WORKSPACE}/spring-mvc-example && mvn clean install"
     }
         }
 
