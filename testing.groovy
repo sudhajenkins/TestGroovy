2398126e99f924ec9ada98ffdc1406df7ed150cd
Devops_CG <sudhaajan14@gmail.com>
2019-05-06 19:36:56 +0530

    comit

--- testing.groovy
+++ testing.groovy
@@ -1,9 +1,5 @@
 #!/usr/bin/env groovy
 
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
+def mvn(args) {
     steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
-}
+	}
