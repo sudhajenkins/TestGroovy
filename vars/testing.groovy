c051516a63f4c2add1d424983780085b6d8aee65
Devops_CG <sudhaajan14@gmail.com>
2019-05-06 22:57:37 +0530

    commint

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 
-def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-	}
+def call(body) {
+    echo "Check status"
+
+    (1..3).each {
+        echo "Number: " + it
+    }
+
+    currentBuild.result = 'SUCCESS' //FAILURE to fail
+    return this
+}
