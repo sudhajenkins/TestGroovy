bdd6310572c9d2e07a89b691b19bffc8d7efc3bb
Devops_CG <sudhaajan14@gmail.com>
2019-05-06 23:31:43 +0530

    test

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,7 +1,28 @@
 #!/usr/bin/env groovy
 
 def call(){
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+
+        // Execute different stages depending on the job
+        if(env.JOB_NAME.contains("deploy")){
+            packageArtifact()
+        } else if(env.JOB_NAME.contains("test")) {
+            buildAndTest()
+        }
+    }
+}
+
+def packageArtifact(){
     stage("Package artifact") {
-        bat "mvn clean"
+        sh "mvn package"
+    }
+}
+
+def buildAndTest(){
+    stage("Backend tests"){
+        sh "mvn test"
     }
 }
